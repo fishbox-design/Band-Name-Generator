788ff74a36b9460c47556bd14b5b8b42ff7806e2
Fishbox_Studio <168686000+fishbox-design@users.noreply.github.com>
2025-01-19 22:19:31 +0100

    Add files via upload

---
+++ bandNameGenerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+print("----------------------------------\n")
+cityName = input("What is the name of your city? ")
+petname = input("What is the name of your favourite pet? ")
+
+print("Your Band Name could be : " + cityName + " " + petname)
+
